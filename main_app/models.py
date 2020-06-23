3af27998a20c045d1ffd0b20098bce7b5bbb32c2
vanessalarsen33 <vanessalarsen33@gmail.com>
2020-06-23 13:21:23 -0600

    nav bar

--- main_app/models.py
+++ main_app/models.py
@@ -25,7 +25,6 @@ ANIMAL_TYPES = (
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     location = models.CharField(max_length=2)
-    avatar_img = models.ImageField(upload_to='images/')
     pet_preference = models.CharField(
         max_length=100,
         choices=ANIMAL_TYPES,

31eb650862fc7d9ec8b679abbc7c4e2e3639f93c
Colin Nelson <colinnelson1996@gmail.com>
2020-06-24 10:46:26 -0600

    get animals busted

--- main_app/views.py
+++ main_app/views.py
@@ -15,7 +15,7 @@ pf = Petfinder(key, secret)
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -42,18 +42,28 @@ def signup(request):
 class ProfileUpdate(UpdateView):
     model = Profile
     fields = ['location', 'pet_preference']
-    success_url = '/rescues/index.html'
+    success_url = '/rescues/'
 
 def get_state_organizations(request):
     profile = Profile.objects.get(id=request.user.id)
     state_organizations = pf.organizations(state=f'{profile.location}')
+    print(state_organizations)
     return render(request, 'rescues/index.html', { 'state_organizations': state_organizations })
 
 def get_animals(request, organization_id):
+    # organization_animals = []
     profile = Profile.objects.get(id=request.user.id)
-    organization_animals = pf.animals(organization_id=organization_id, animal_type=f'{profile.pet_preference}')
+    organization = organization_id
+    animals = pf.animals(animal_type=f'{profile.pet_preference}')
+    # organization_filter = animals.animals.
+    organization_animals = [animal['organization_id'] == organization_id for animal in animals['animals']]
+    print(profile.pet_preference)
+    print(type(organization_id))
+    print(type(organization))
+    print(organization_id)
     print(organization_animals)
-    return render(request, 'rescues/rescue_detail.html') #{ 'organization_animals': organization_animals })
+    print(animals)
+    return render(request, 'rescues/rescue_detail.html', { 'organization_animals': organization_animals, 'organization_id': organization_id })
 
 def get_animal_details(request, animal_id):
     pass
